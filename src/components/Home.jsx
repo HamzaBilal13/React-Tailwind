1e0b6b604603dcb1357fcd554bfedb3a0f340aa6
HamzaBilal13 <hamzabilal13>
2023-05-07 13:02:47 +0500

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="bg-[red] color-[white] text-3xl text-center py-4">Welcome To Home</h1>
-         </div>
+    // <div className="w-full h-screen" style={{}}>
+    //   <h1 className="color-[white] text-3xl text-center py-4">Welcome To Home</h1>
+    // </div>
+    <div className="container lg:columns-2 md:columns-2 py-[5%]">
+      <div className="">
+        <h1 className="font-bold px-[10%] text-2xl pb-3">Hi I am Hamza Bilal</h1>
+        <h1 className="font-bold px-[10%] text-2xl pb-6">I love to build amazing websites in Reactjs</h1>
+        <p className="px-[10%]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+
+       <div className="px-[20%]">
+       <div class="px-12 py-8 bg-white max-w-sm rounded overflow-hidden shadow-lg">
+          <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+      </div>
+       </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,63 +7,72 @@ import Contact from './Contact';
 import Login from './Login';
 import Form2 from './Form2';
 import { Routes, Route, Link } from 'react-router-dom';
+import { MdLogin } from "react-icons/md";
 
 
 const Navbar = () => {
-  const[showMyModal, setShowMyModal] = useState(false);
+  const [showMyModal, setShowMyModal] = useState(false);
   const handleOnClose = () => setShowMyModal(false);
   return (
-  <header className="absolute inset-x-0 top-0 z-50">
-    <nav className="flex items-center justify-between p-6 lg:px-8 border-b shadow" aria-label="Global">
-      <div className="flex lg:flex-1">
-      
+
+    <header className="absolute inset-x-0 top-0 z-50">
+
+      <nav className="bg-white flex items-center justify-between p-4 lg:px-8 border-b shadow" aria-label="Global">
+        <div className="flex lg:flex-1">
+
           <h1 className="font-medium">Hamza Bilal</h1>
-      </div>
-      <div className="flex lg:hidden">
-        <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-          <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
+        </div>
+        <div className="flex lg:hidden">
+          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">Home</Link>
+          <Link to="/Gallery" className="text-sm font-semibold leading-6 text-gray-900">Portfolio</Link>
+          <Link to="/Services" className="text-sm font-semibold leading-6 text-gray-900">Services</Link>
+          <Link to="/About" className="text-sm font-semibold leading-6 text-gray-900">About</Link>
+          <Link to="/Contact" className="text-sm font-semibold leading-6 text-gray-900">Contact</Link>
+
+        </div>
+        <button class="btn btn-blue hidden lg:flex lg:flex-1 lg:justify-end">
+
+          <a className="text-sm font-semibold leading-6 bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">Signup
+            <span aria-hidden="true">
+              &rarr;
+            </span>
+          </a>
+
+
         </button>
-      </div>
-      <div className="hidden lg:flex lg:gap-x-12">
-        <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">Home</Link>
-        <Link to="/Gallery" className="text-sm font-semibold leading-6 text-gray-900">Gallery</Link>
-        <Link to="/Services" className="text-sm font-semibold leading-6 text-gray-900">Services</Link>
-        <Link to="/About" className="text-sm font-semibold leading-6 text-gray-900">About</Link>
-        <Link to="/Contact" className="text-sm font-semibold leading-6 text-gray-900">Contact</Link>
-
-      </div>
-      <button class="btn btn-blue hidden lg:flex lg:flex-1 lg:justify-end" onClick={ ()=> setShowMyModal(true)}>
-      
-        <a className="text-sm font-semibold leading-6 bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login
-        <span aria-hidden="true">&rarr;</span>
-        </a>
-      
-
-      </button>
-      {/* <Login show={showLogin} close={() => setShowLogin(false)} /> */}
-      < Login closeModal={handleOnClose} showModal={showMyModal}/>
-
-    </nav>
-    <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/Gallery" element={<Gallery />}></Route>
-                <Route path="/Services" element={<Services />}></Route>
-                <Route path="/About" element={<About />}></Route>
-                <Route path="/Contact" element={<Contact />}></Route>
-                <Route path="/Login" element={<Login />}></Route>
-                <Route path="/Form2" element={<Form2 />}></Route>
-    
-            </Routes>
 
 
-    
-
-  </header>
 
 
-  
+
+        < Login closeModal={handleOnClose} showModal={showMyModal} />
+
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Gallery" element={<Gallery />}></Route>
+        <Route path="/Services" element={<Services />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Form2" element={<Form2 />}></Route>
+
+      </Routes>
+
+
+
+
+    </header>
+
+
+
 
   );
 }
